7972976998274910df3611bb2e4c7568905d6bb1
Travis Swicegood <development@domain51.com>
2010-01-24 13:13:14 -0600

    Add @task decorator that works in all aspects
    Finally landed on a design.  This uses the Task class as a signifier that the
    variable in the fabfile module is a Task.  It provides a WrappedCallableTask to
    provide the basic decorator syntax, but still leaves open the possibility for
    additional Task objects that provide additional behavior.

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -2,6 +2,9 @@ from nose.tools import ok_
 
 from fabric import decorators, tasks
 
-def test_task_is_an_instance_of_task_object():
-    ok_(isinstance(decorators.task, tasks.Task))
+def test_task_returns_an_instance_of_wrappedfunctask_object():
+    def foo():
+        pass
+    task = decorators.task(foo)
+    ok_(isinstance(task, tasks.WrappedCallableTask))
 
